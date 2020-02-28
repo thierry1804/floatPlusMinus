a7c42cf72edaf86fe837fec07057d2ad90b55d73
thierry1804 <thierry1804@gmail.com>
2020-02-28 22:36:14 +0300

    First commit

---
+++ public_html/js/test.js
@@ -0,0 +1,28 @@
+$('#test').off('keypress').on('keypress', function(e) {
+    var regex = RegExp(/-|\.|\d/);
+    if (!regex.test(e.key)) {
+        e.preventDefault();
+    }
+    regex = RegExp(/\./);
+    if (regex.test($(this).val()) && e.key === '.') {
+        e.preventDefault();
+    }
+    regex = RegExp(/-/);
+    if (regex.test($(this).val()) && e.key === '-') {
+        e.preventDefault();
+    }
+    regex = RegExp(/^0/);
+    var regexN = RegExp(/\./);
+    if (regex.test($(this).val()) && !regexN.test(e.key) && e.target.selectionStart === 1) {
+        e.preventDefault();
+    }
+    regex = RegExp(/^-/);
+    regexN = RegExp(/\d/);
+    if (regex.test($(this).val()) && !regexN.test(e.key) && e.target.selectionStart === 1) {
+        e.preventDefault();
+    }
+    regex = RegExp(/^-0/);
+    if (regex.test($(this).val()) && e.key !== '.' && e.target.selectionStart === 2) {
+        e.preventDefault();
+    }
+});
