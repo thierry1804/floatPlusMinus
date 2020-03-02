1ef2d0c31b5dacf62913ae31b2b315edc2edd691
Thierry <thierry1804@users.noreply.github.com>
2020-03-02 10:44:47 +0300

    Correction pour positif uniquement

--- public_html/js/test.js
+++ public_html/js/test.js
@@ -1,5 +1,5 @@
 $('#test').off('keypress').on('keypress', function(e) {
-    var regex = RegExp(/-|\.|\d/);
+    var regex = RegExp(/\.|\d/);
     if (!regex.test(e.key)) {
         e.preventDefault();
     }
@@ -7,27 +7,13 @@ $('#test').off('keypress').on('keypress', function(e) {
     if (regex.test($(this).val()) && e.key === '.') {
         e.preventDefault();
     }
-    if (e.key === '-' && e.target.selectionStart !== 0) {
-        e.preventDefault();
-    }
-    if (e.key !== '-' && e.target.selectionStart === 0) {
-        e.preventDefault();
-    }
-    regex = RegExp(/-/);
-    if (regex.test($(this).val()) && e.key === '-') {
-        e.preventDefault();
-    }
-    regex = RegExp(/^-/);
-    var regexN = RegExp(/\d/);
+    regex = RegExp(/^0/);
+    var regexN = RegExp(/\./);
     if (regex.test($(this).val()) && !regexN.test(e.key) && e.target.selectionStart === 1) {
         e.preventDefault();
     }
-    regex = RegExp(/^-\d/);
-    if (regex.test($(this).val()) && e.key === '0' && e.target.selectionStart === 1) {
-        e.preventDefault();
-    }
-    regex = RegExp(/^-0/);
-    if (regex.test($(this).val()) && e.key !== '.' && e.target.selectionStart === 2) {
+    regex = RegExp(/\d\./);
+    if (regex.test($(this).val()) && e.key === '0' && e.target.selectionStart === 0) {
         e.preventDefault();
     }
 });
